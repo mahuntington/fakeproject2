cf359385de9fc3104dc420be159fa457485f7d57
Matt Huntington <matt.huntington@gmail.com>
2018-07-06 10:44:34 -0400

    express running

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('this works');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('this works');
+})
+
+app.listen(3000, ()=>{
+    console.log('listening...');
+})
