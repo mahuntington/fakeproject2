d9fcf22c77c86f643b036fcff7c158c1dfae3f39
Matt Huntington <matt.huntington@gmail.com>
2018-07-06 11:51:36 -0400

    installing mongoose

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoose = require('mongoose');
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+
 app.get('/', (req, res)=>{
     res.send('this works');
 })
 
 app.listen(PORT, ()=>{
     console.log('listening...');
+})
+
+mongoose.connect(mongoUri);
+mongoose.connection.on('open', ()=>{
+    console.log('connected to mongoose!!!!!!');
 })
